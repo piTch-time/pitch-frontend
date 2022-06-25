7fea483d0833a030f19f6018b45c507d9eff8be2
Bonjun Ku <ku96kbj@naver.com>
2022-06-25 19:34:37 +0900

    feat : Room 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { GlobalStyle } from '@sharedComponents/GlobalStyle';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Home } from '@pages/Home';
 import { SamplePage } from '@pages/SamplePage';
+import { Room } from '@pages/Room/Room';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/samplepage' element={<SamplePage />} />
+        <Route path='/room' element={<Room />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Room/Room.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Room = () => {
+  return (
+    <Container>
+      <h1>This is Room.</h1>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: green;
+`;
